64dd0c791ca49c55d2396eff0f08b0e071be927b
Иван <johnny.kiv@yandex.ru>
2024-02-10 13:29:56 +0300

    task2

--- task1/task1.py
+++ task1/task1.py
@@ -1,14 +1,27 @@
+import random
+alph = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 inp = open("input.txt")
 out = open("output.txt", "w")
 f = inp.read().split("\n")
-for s in range(len(f)):
-    l = f[s]
+kol = int(f[0])
+min = int(f[1])
+max = int(f[2])
+w = f[3]
+stat = [0,1]
+for i in range(kol):
+    x = random.randint(min,max)
+    line = ''
     n = 0
-    for i in range(len(l)):
-        if l[i] == "A" and l[i+1]=="B":
-           n+=1
-    out.write(str(n)+"\n")
+    for i in range(x):
+        y = random.choice(stat)
+        if y==stat[0] and n!=10:
+            line+=w
+            x-=3
+            n+=1
+        else:
+            line +=random.choice(alph)
 
+    out.write(line+"\n")
 out.close()
 inp.close()
 
--- task2/task2.py
+++ task2/task2.py
@@ -5,7 +5,7 @@ for s in range(len(f)):
     l = f[s]
     n = 0
     for i in range(len(l)):
-        if l[i] == "A" and l[i+1]=="B":
+        if l[i] == "C" and l[i+1]=="D" and l[i+2]=="A":
            n+=1
     out.write(str(n)+"\n")
 

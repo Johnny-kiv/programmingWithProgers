b828abd87658527b708fe122db20d9e577455999
Иван <johnny.kiv@yandex.ru>
2024-02-10 12:40:14 +0300

    iinit

--- task1/task1.py
+++ task1/task1.py
@@ -1,7 +1,11 @@
 inp = open("input.txt")
 out = open("output.txt", "w")
-l = inp.read()
-out.write(str(l.count("AB")))
+l = list(inp.read())
+n = 0
+for i in range(len(l)):
+    if l[i] == "A" and l[i+1]=="B":
+       n+=1
+out.write(str(n))
 out.close()
 inp.close()
 

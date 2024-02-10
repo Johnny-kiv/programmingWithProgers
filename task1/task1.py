4d79fd888b6d48e7c8f65320728586b1572a0db2
Иван <johnny.kiv@yandex.ru>
2024-02-10 12:48:33 +0300

    iinit

--- task1/task1.py
+++ task1/task1.py
@@ -1,11 +1,14 @@
 inp = open("input.txt")
 out = open("output.txt", "w")
-l = list(inp.read())
-n = 0
-for i in range(len(l)):
-    if l[i] == "A" and l[i+1]=="B":
-       n+=1
-out.write(str(n))
+f = inp.read().split("\n")
+for s in range(len(f)):
+    l = f[s]
+    n = 0
+    for i in range(len(l)):
+        if l[i] == "A" and l[i+1]=="B":
+           n+=1
+    out.write(str(n)+"\n")
+
 out.close()
 inp.close()
 
